8d3393673a0f23bedb21fe75cd22cf949778ffd9
Vahtangius <vahtangi@i.com>
2021-07-12 14:07:20 +0300

    Changed style in About.module.css
    1. Added different padding for second title in About.jsx
    2. Refactored About.module.css: changed px to em,added one class

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -10,11 +10,10 @@ const About = () => {
             <h2 className={s.aboutBody__title}>Who we are</h2>
             <p>We created this service together all the popular vinyl record stores</p>
             <p>at one place and make your shopping cheaper.</p>
-            <br/>
-            <h2 className={s.aboutBody__title + ' ' + s.aboutImage}>What you can do</h2>
+            <h2 className={s.aboutBody__title + ' ' + s.aboutBody__secondTitle}>What you can do</h2>
             <p>You can search through various stores, compare prices,</p>
             <p>add records to your wantlist and get notification when the price drops.</p>
-            <div className={s.aboutImage}>
+            <div className={s.aboutBody__image}>
               <img src={aboutImage} alt="vinyl"/>
             </div>
           </div>
