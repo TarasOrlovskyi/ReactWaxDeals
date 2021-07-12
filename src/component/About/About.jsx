34524116f33b20094808bba827ca972a59e9f960
Vahtangius <vahtangi@i.com>
2021-07-12 13:29:32 +0300

    Changed after review

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -6,14 +6,12 @@ const About = () => {
         <main className="main">
             <div className="container subContainer">
                 <div className={s.aboutContent}>
-
                     <div className={s.aboutBody}>
                         <h2 className={s.aboutBody__title}>Who we are</h2>
                         <p>We created this service together all the popular vinyl record stores</p>
                         <p>at one place and make your shopping cheaper.</p>
                         <br/>
-                        <br/>
-                        <h2 className={s.aboutBody__title}>What you can do</h2>
+                        <h2 className={s.aboutBody__title + ' ' + s.aboutImage}>What you can do</h2>
                         <p>You can search through various stores, compare prices,</p>
                         <p>add records to your wantlist and get notification when the price drops.</p>
                         <div className={s.aboutImage}>
