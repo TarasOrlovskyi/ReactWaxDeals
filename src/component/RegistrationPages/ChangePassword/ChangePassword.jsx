fb321218853f063e615a40e7c54fe1a2289a3cfc
Vahtangius <vahtangi@i.com>
2021-07-16 00:40:44 +0300

    Commited after review
    1. In EditProfile.jsx moved image to svg file and imported
    2. Renamed and imported s to editeProfileStyle
    3. In EditProfile.jsx and ChangePassword.jsx delete class main in teg
    main
    4. In ChangePassword.module.css change width px to em
    5. In EditProfile.jsx deleted empty row

--- src/component/RegistrationPages/ChangePassword/ChangePassword.jsx
+++ src/component/RegistrationPages/ChangePassword/ChangePassword.jsx
@@ -1,4 +1,4 @@
-import s from './ChangePassword.module.css';
+import editeProfileStyle from './ChangePassword.module.css';
 import userStyle from '../User.module.css';
 import UserMessage from "../UserMessage/UserMessage";
 
@@ -6,27 +6,27 @@ const ChangePassword = (props) => {
   document.body.style.background = "url(assets/background.jpg) no-repeat fixed center";
   document.body.style.backgroundSize = "cover";
   return (
-    <main className="main">
+    <main>
       <div className="container">
         <div className="searchArea">
         </div>
-        <div className={s.contentChangePassword + ' content'}>
+        <div className={editeProfileStyle.contentChangePassword + ' content'}>
           <UserMessage/>
-          <div className={s.contentChangePassword__column + ' contentColumn'}>
+          <div className={editeProfileStyle.contentChangePassword__column + ' contentColumn'}>
             <div className={userStyle.contentUser__title}>
               <h2>Change Password</h2>
             </div>
             <form action="/password/changePassword" id="changePassword-form" method="POST"
-                  className={userStyle.user + ' ' + s.changePasswordUser} name="form_edit"
+                  className={userStyle.user + ' ' + editeProfileStyle.changePasswordUser} name="form_edit"
                   onSubmit="return matchPassword();">
               <div className={userStyle.user__fieldsWrapper}>
-                <div className={userStyle.user__fields + ' ' + s.changePasswordUser__fields}>
+                <div className={userStyle.user__fields + ' ' + editeProfileStyle.changePasswordUser__fields}>
                   <label className={userStyle.user__label}>
                     <input className={userStyle.user__input} type="password" name="oldPassword"
                            placeholder="Old password"
                            required/>
                   </label>
-                  <div className={s.changePasswordUser__border}></div>
+                  <div className={editeProfileStyle.changePasswordUser__border}></div>
                   <label className={userStyle.user__label}>
                     <input className={userStyle.user__input} type="password" name="newPassword"
                            placeholder="New password"
