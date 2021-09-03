eebffb98085a2cbe7bf36b637c29632590e1a174
Vahtangius <vahtangi@i.com>
2021-09-03 11:35:02 +0300

    Improved all mistakes
    1. In RectangleLine.jsx
    2. In RectangleLine.module.css

--- src/component/RegistrationPages/SignIn/RectangleLine/RectangleLine.jsx
+++ src/component/RegistrationPages/SignIn/RectangleLine/RectangleLine.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import rectangleLineStyle from "./RectangleLine.module.css";
-import rectangleLine from "../../../../assets/img/socialNetwork/Rectangle-line.png";
 
 const RectangleLine = () => {
   return (
-      <div className={rectangleLineStyle.contentSignIn__rectangleLine + ' rectangleLine'}>
+      <div className={rectangleLineStyle.contentSignIn__rectangleLine}>
         <span>or</span>
       </div>
   );
