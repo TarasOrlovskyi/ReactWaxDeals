ce99aa63d3b432062dc95924caf807ad7a6c7f7c
Vahtangius <vahtangi@i.com>
2021-09-11 20:42:07 +0300

    Home page
    1. Mobile layout
    2. Laptop version

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,29 +1,33 @@
 import s from './Home.module.css';
 import SearchFieldContainer from "../SearchField/SearchFieldContainer";
+import waxDealsLogo from "../../assets/img/Wax_Deals_logo_mobile.png";
 
 const Home = () => {
   return (
-    <main className="main">
-      <div className="container">
-        <div className="searchArea">
-        </div>
-        <div className={s.contentHome + ' content'}>
-          <div className={s.contentHome__column + ' contentColumn'}>
-            <div className={s.contentHome__title}>
-              <h1>COMPARE PRICES of VINYL RECORDS</h1>
+      <main className="main">
+        <div className="container">
+          <div className="searchArea">
+          </div>
+          <div className={s.contentHome + ' content'}>
+            <div className={s.contentHome__logo}>
+              <img src={waxDealsLogo} alt="Wax Deals logo"/>
             </div>
-            <SearchFieldContainer/>
-            <div>
-              <h2>
-                You can find here a huge number of records from<br/>
-                trusted stores, so you can find the best price<br/>
-                Welcome!
-              </h2>
+            <div className={s.contentHome__column + ' contentColumn'}>
+              <div className={s.contentHome__title}>
+                <h1>COMPARE PRICES of VINYL RECORDS</h1>
+              </div>
+              <SearchFieldContainer/>
+              <div className={s.contentHome__subtitle}>
+                <h2>
+                  You can find here a huge number of records from<br/>
+                  trusted stores, so you can find the best price<br/>
+                  Welcome!
+                </h2>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
   );
 }
 
