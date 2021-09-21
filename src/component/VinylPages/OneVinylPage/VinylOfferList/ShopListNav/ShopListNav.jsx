0cfb82749bc347c2ae83406295699f6befa8b121
Bogdan Parashchak <bogdan.parashchak@gmail.com>
2021-09-21 22:47:17 +0300

    Layout all pages according design
    1. Comments deleted.

--- src/component/VinylPages/OneVinylPage/VinylOfferList/ShopListNav/ShopListNav.jsx
+++ src/component/VinylPages/OneVinylPage/VinylOfferList/ShopListNav/ShopListNav.jsx
@@ -5,7 +5,7 @@ import DiscogsLink from "./DiscogsLink/DiscogsLink";
 
 const ShopListNav = (props) => {
   return (
-    <div className={/*vinylOfferListStyle.shopList__row + ' ' + */shopListNavStyle.shopList__rowFirstTitle}>
+    <div className={shopListNavStyle.shopList__rowFirstTitle}>
       <div className={shopListNavStyle.shopList__item + ' ' + shopListNavStyle.shopList__firstItemTitle}>
         <div className={vinylOfferListStyle.shopList__title + ' ' + shopListNavStyle.shopList__menuItem_active}>
           Prices
